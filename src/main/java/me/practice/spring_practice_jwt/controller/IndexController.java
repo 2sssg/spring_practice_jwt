7782f1c7e1fd404930c7861984250735c56f8b4c
2sssg <lsg020300@naver.com>
2022-10-29 15:58:47 +0900

    feat: index.html file 및 controller 추가

--- src/main/java/me/practice/spring_practice_jwt/controller/IndexController.java
+++ src/main/java/me/practice/spring_practice_jwt/controller/IndexController.java
@@ -1,2 +1,22 @@
-package me.practice.spring_practice_jwt.controller;public class IndexController {
+package me.practice.spring_practice_jwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+
+	/**
+	 * 인덱스 페이지 리턴하는 리스너
+	 * @return index.html
+	 */
+	@GetMapping({"", "/"})
+	public String index() {
+		// mustache deault directory
+		// src/main/resources
+		// view resolver config : templates (prefix), .mustache (suffix) // 생략가능!
+		return "index";
+	}
+
 }
