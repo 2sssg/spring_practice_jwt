ab720ae643729ce5e19812a5b7e625ea56e1c893
2sssg <lsg020300@naver.com>
2022-10-29 16:20:21 +0900

    feat: security config and login.html

--- src/main/java/me/practice/spring_practice_jwt/config/SecurityConfig.java
+++ src/main/java/me/practice/spring_practice_jwt/config/SecurityConfig.java
@@ -1,2 +1,32 @@
-package me.practice.spring_practice_jwt.config;public class SecurityCOnfig {
+package me.practice.spring_practice_jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+				.csrf().disable()
+				.authorizeRequests()
+					.antMatchers("/user/**").authenticated()
+					.antMatchers("/manager/**")
+						.access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+					.antMatchers("/admin/**")
+						.hasRole("ADMIN")
+					.anyRequest()
+						.permitAll();
+
+		httpSecurity.formLogin().loginPage("/login");
+
+
+		return httpSecurity.build();
+	}
+
 }
--- src/main/java/me/practice/spring_practice_jwt/controller/IndexController.java
+++ src/main/java/me/practice/spring_practice_jwt/controller/IndexController.java
@@ -2,6 +2,7 @@ package me.practice.spring_practice_jwt.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -17,6 +18,36 @@ public class IndexController {
 		// src/main/resources
 		// view resolver config : templates (prefix), .mustache (suffix) // 생략가능!
 		return "index";
+	}
+
+	@GetMapping("/user")
+	public String user() {
+		return "user";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "admin";
+	}
+
+	@GetMapping("/manager")
+	public String manager() {
+		return "manager";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/join")
+	public String join() {
+		return "join";
+	}
+
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료!";
 	}
 
 }
