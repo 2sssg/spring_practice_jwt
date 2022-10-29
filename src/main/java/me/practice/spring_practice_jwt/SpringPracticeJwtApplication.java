e758db9900ca198f05a65a8f319fe8b174ebc020
2sssg <lsg020300@naver.com>
2022-10-29 15:40:00 +0900

    first init

---
+++ src/main/java/me/practice/spring_practice_jwt/SpringPracticeJwtApplication.java
@@ -0,0 +1,13 @@
+package me.practice.spring_practice_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/practice/spring_practice_jwt/SpringPracticeJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package me.practice.spring_practice_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
